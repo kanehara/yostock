db55504b4caa57c7a7cecbdbf2a1c48f4bd8ed35
yohei.kanehara <yohei.kanehara@vodori.com>
2017-03-19 01:51:14 -0500

    Made init middleware functionˆ

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,2 +1,25 @@
-export * from './auth';
-export * from './dev';
+import setupDevMiddleware from './dev';
+import logger from './logger';
+import cookieParser from "cookie-parser";
+import auth from './auth';
+
+/**
+ * Initializes Express middleware
+ * @param app - Express app instance
+ */
+export default function init(app) {
+
+  // Webpack dev server
+  if (process.env.NODE_ENV === 'development') {
+    setupDevMiddleware(app);
+  }
+
+  // Logging
+  app.use(logger);
+
+  // Cookie parser
+  app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
+
+  // Setup authorization
+  app.use(auth);
+}
