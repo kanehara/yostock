c72bbd9a901eed7f5add73164c46165b30d93145
yohei.kanehara <yohei.kanehara@vodori.com>
2017-03-22 22:33:13 -0500

    Changed webpack middleware to point to refactored ˆwebpack.config.dev.js

--- server/middleware/dev.js
+++ server/middleware/dev.js
@@ -1,10 +1,10 @@
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpack from 'webpack';
-import webpackConfig from '../../config/webpack.config.client.js';
+import webpackConfigDev from '../../client/config/webpack.config.dev.js';
 
 export default function setupDevMiddleware(app) {
-  const compiler = webpack(webpackConfig);
+  const compiler = webpack(webpackConfigDev);
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: webpackConfig.output.publicPath
